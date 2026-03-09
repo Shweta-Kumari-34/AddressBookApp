86e7f3bd1ec3b557c208f290f15b44bac46f0040
unknown <shwetakumari3405@gmail.com>
2026-03-09 15:11:43 +0530

    [shweta]: Created Address Book Application program

---
+++ src/main/java/com/addressbookapp/AddressBookApplication.java
@@ -0,0 +1,14 @@
+package com.addressbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookApplication.class, args);
+		System.out.println("Welcome to Address book Application");
+	}
+
+}
